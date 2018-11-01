7fa0a30560dbd377fba3cf166efc8970fdfcd062
yaoxiang <1412760992@qq.com>
2018-11-01 19:09:28 +0800

    netty使用demo

---
+++ src/main/java/com/yao/nettydemo/handler/ServerHandler.java
@@ -0,0 +1,28 @@
+package com.yao.nettydemo.handler;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+@ChannelHandler.Sharable
+public class ServerHandler extends ChannelInboundHandlerAdapter {
+    /**
+     *
+     * @param ctx
+     * @param msg
+     * @throws Exception
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        super.channelRead(ctx, msg);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        super.channelReadComplete(ctx);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        super.exceptionCaught(ctx, cause);
+    }
+}
